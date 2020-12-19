b8e7b6c1b85be6a2706d226f39d6d63565b6f212
lancelotzx <lancelotzx@163.com>
2020-12-20 04:36:03 +0800

    [feature]添加缴费历史记录-组件UI适配

--- components/orderInfo.vue
+++ components/orderInfo.vue
@@ -1,47 +1,52 @@
+<!--
+@author:wangjia
+@date:20201220
+@description: 缴费历史记录的单元数据组件，从父组件传递的obj应该包含
+   1. 缴款的收款方中文名称
+   2. 市区、区县标志
+   3. 缴款金额
+   4. 缴款日期 时间
+
+-->
 <template>
 	<view>
 		<view class="order-info">
 			<u-row :gutter="0">
-				<u-col :span="6" align="left">
-					<u-tag type="info" text="图文专问" shape="circle" mode="dark"></u-tag>
+				<u-col :span="9" align="left">
+					<text class="order-receiver-name">茂名市教育局学生服务中心茂名市教育局学生服务中心</text>
 				</u-col>
-				<u-col :span="6" align="right">
+				<u-col :span="3" align="right">
 					<view style="text-align: right;">
-						<u-tag :type="statusFormat[0].type" :text="statusFormat[0].title" shape="circle"></u-tag>
+						<u-tag type="info" text="市级" shape="circle"></u-tag>
 					</view>
 				</u-col>
 			</u-row>
 			<view style="margin-top:15rpx">
 				<u-row :gutter="0">
-					<u-col :span="10">
-						<text class="order-remark">我想请问一下没有户口本可以登记结婚吗？</text>
-					</u-col>
+					
 					<u-col :span="2">
 						<view style="text-align: center;">
-							<text class="order-price">￥199</text>
+							<text class="order-price">￥199912.00</text>
+						</view>
+					</u-col>
+					<u-col :span="3" :offset="6">
+						<view style="text-align: right;">
+							<image src="/static/images/his-03.png" mode="widthFix" style="width:40%; display: block; position: absolute;">
+							</image>
 						</view>
 					</u-col>
 				</u-row>
 			</view>
-			<view style="margin-top:40rpx;">
+			<view style="margin-top:16rpx;">
 				<u-row :gutter="0">
 					<u-col :span="6">
 						<u-row :gutter="0">
-							<u-col :span="2">
-								<u-avatar src="http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg" size="50"></u-avatar>
-							</u-col>
 							<u-col :span="10">
-								<text class="order-lawyer-name">张三律师 婚姻家庭</text>
+								<text class="order-datetime">2020/12/20 10:00:00</text>
 							</u-col>
 						</u-row>
 					</u-col>
-					<u-col :span="6">
-						<view style="text-align: right;">
-							<view>
-								<u-button size="mini" shape="circle" :ripple="true">立即支付</u-button>
-							</view>
-						</view>
-					</u-col>
+					
 				</u-row>
 			</view>
 		</view>
@@ -85,17 +90,34 @@
 		background-color: white;
 		border:1rpx solid white;
 		border-radius:20rpx;
-		padding:25rpx;
+		padding:22rpx;
 		margin-bottom:20rpx;
 	}
 	.order-remark{
 		font-size:30rpx;
 	}
 	.order-price{
-		color:red;
-		font-size:30rpx;
+		font-size: 43rpx;
+		font-family: SourceHanSansSC-Regular;
+		line-height: 50rpx;
+		color: #FF3952;
+		
+		opacity: 1;
 	}
-	.order-lawyer-name{
-		font-size:25rpx;
+	.order-datetime{
+		font-size: 23rpx;
+		font-family: SourceHanSansSC-Regular;
+		line-height: 31rpx;
+		color: #ACB0B7;
+		letter-spacing: 0rpx;
+		opacity: 1;
+	}
+	.order-receiver-name{
+		font-size: 28rpx;
+		font-family: SourceHanSansSC-Regular;
+		line-height: 30rpx;
+		color: #555555;
+		//letter-spacing: 20rpx;
+		opacity: 1;
 	}
 </style>
