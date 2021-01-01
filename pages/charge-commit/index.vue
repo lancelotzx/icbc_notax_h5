b0691548cc5b40b7117251c4525703c02275f4a9
lancelotzx <lancelotzx@163.com>
2021-01-02 02:26:58 +0800

    [feature]添加缴费提交后的icbc返回html显示页面

---
+++ pages/charge-commit/index.vue
@@ -0,0 +1,128 @@
+<!--
+@author:wangjia
+@date:20210101
+@description: 进行缴费提交页面，本页面显示为icbc sdk返回的html代码进行的直接显示
+              机制：通过web-view显示html raw code内容，html代码为icbc sdk ui类接口返回的动态代码，
+			  需要放置在tencent云服务器 /usr/local/nginx/html/h5的某位置，或springboot可访问的某位置，
+			  通过nginx 的root来做位置代理。配置完成后，springboot生成的动态文件（文件名需要唯一）通过
+			  web-view直接显示在用户移动前端。
+			  为了调试需要先将root配置为www.onetwo1.top/inde.html
+-->
+
+<template>
+	<view>
+		
+		<view style="width:100%;" >
+			<!--wangjia:使用absolute position可以充当背景-->
+			<view  style="margin:0;padding:0;">
+				<image src="/static/images/ma-01.png" mode="widthFix" style="width:100%; display: block;position: absolute;
+				  z-index: -1;">
+				</image>
+			</view>
+			<u-row :gutter="1" >
+				<u-col  :offset="1">
+					<view style="margin:0;padding:0;padding-top: 76rpx;" @click="goBack">
+						<image src="/static/images/his-02.png" mode="widthFix" style="width:4%; display: block;position: absolute;
+					"></image>
+					</view>
+				</u-col>
+				
+				<u-col  :offset="3.5">
+					<view style="margin:0;padding:0;  padding-bottom: 76rpx; z-index: 2;" >
+						<view>
+							<text class="his-title">缴费提交</text>
+						</view>
+					</view>
+				</u-col>
+			</u-row>
+			
+			
+			
+		</view>
+		<view class="bg-subtitle">
+			
+			<web-view src="http://www.onetwo1.top/"></web-view>
+		</view>
+
+		
+	</view>
+</template>
+
+<script>
+	import orderInfo from '@/components/orderInfo.vue';
+	
+	export default {
+		components:{
+			orderInfo
+		},
+		
+		data() {
+			return {
+				
+			};
+		},
+		onShow(){
+			let _this = this;
+			
+		},
+		
+		methods:{
+		}
+	}
+</script>
+
+<style lang="scss">
+	.wrap {
+		padding: 30rpx;
+	}
+	.bg-subtitle {
+		width: 750rpx;
+		height: 900rpx;
+		position: absolute;
+		padding-top: 10rpx;
+		padding-left: 40rpx;
+		
+		background: #FFFFFF;
+		box-shadow: 0px 2px 24px rgba(0, 0, 0, 0.08);
+		opacity: 1;
+		border-radius: 50rpx;
+	}
+	.scroll-Y {
+			height: 1000rpx;
+		}
+	.order-list-bg{
+		background-color: #eef5fe;
+		padding-top:20rpx;
+		padding-left:15rpx;
+		padding-right:15rpx;
+		padding-bottom:10rpx;
+		margin-top:10rpx;
+	}
+	
+    .his-title{
+		
+		font-size: 23px;
+		font-family: SourceHanSansSC-Regular;
+		//line-height: 53px;
+		color: #FFFFFF;
+		opacity: 1;
+	}	
+	
+	.his-main-bg{
+		
+	}
+	
+	.his-year-selector{
+		position: relative;
+		padding-top:  10rpx;
+		padding-left: 100rpx;
+		font-size: 30rpx;
+		font-family: SourceHanSansSC-Regular;
+		line-height: 48px;
+		color: #353535;
+		opacity: 1;
+		z-index: 2;
+
+	}
+	
+</style>
